0c43843b9420f8fe53b6516614f41ffbde5a3525
s5163577 <100301188+s5163577@users.noreply.github.com>
2022-04-03 21:41:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@ int main(){
     cin >> n;
    
     if (n>=2) {
-       for (i=1;i<=n;i++){
-           c=a+b
-           a=b;
-           b=c;
-           cout << c << endl;
-       }
-   } else }
-       cout << "errore" << endl;
-   }
-   return 0;
+        for (i=1;i<=n;i++){
+            c=a+b
+            a=b;
+            b=c;
+            cout << c << endl;
+        }
+    } else }
+        cout << "errore" << endl;
+    }
+    return 0;
 }
